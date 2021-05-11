1dc5a5a7cd3737e69ee20db15f93b74f97bd0e4e
chyuwei <weichangyuwcy@gmail.com>
2021-05-11 16:13:56 +0800

    add language config

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -18,6 +18,5 @@
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 
-
 (provide 'early-init)
 ;;; early-init.el ends here
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,7 +4,6 @@
 ;;
 ;;; Code:
 
-
 (add-to-list 'load-path (expand-file-name "core" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "editor" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "langs" user-emacs-directory))
@@ -12,7 +11,6 @@
 (require 'init-core)
 (require 'init-ui)
 (require 'init-editor)
-
+(require 'init-langs)
 
 (provide 'init)
-;;; init.el ends here
