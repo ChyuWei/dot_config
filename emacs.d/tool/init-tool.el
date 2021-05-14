33a4fc0eb4944d0e17af796a21b25a4b1ffce877
fishsalter <weichangyuwcy@gmail.com>
2021-05-14 17:52:18 +0800

    add rime

--- emacs.d/tool/init-tool.el
+++ emacs.d/tool/init-tool.el
@@ -8,4 +8,11 @@
   :ensure t
   :init (setq magic-diff-refine-hunk t))
 
+(use-package rime
+  :ensure t
+  :custom
+  ((default-input-method "rime")
+   (rime-show-candidate `posframe)
+   (rime-posframe-style `vertical)))
+
 (provide 'init-tool)
