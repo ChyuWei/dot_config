e3cfe8ee51ae314b72d7edc5e3f029a1a495d943
chyuwei <625388793@qq.com>
2021-05-30 16:39:03 +0800

    emacs config install

--- install.sh
+++ install.sh
@@ -46,25 +46,45 @@ backup_dir() {
 install_vim() {
     echo "install vim"
 
-    VIMRC_PATH="$HOME/.vimrc"
-    VIMDIR_PATH="$HOME/.vim"
-    VIM_SRC="$(pwd)/vimrc"
+    SRC_PATH="$(pwd)/vimrc"
+    DST_PATH="${HOME}/.vimrc"
+    DST_DIR_PATH="${HOME}/.vim"
 
-    check_already $VIMRC_PATH $VIM_SRC
+    check_already $DST_PATH $SRC_PATH
     if [ $? -eq 1 ]
     then 
         echo "already install"
         return
     fi
 
-    backup_file $VIMRC_PATH ".vimrc"
-    backup_dir $VIMDIR_PATH ".vim"
+    backup_file $DST_PATH ".vimrc"
+    backup_dir $DST_DIR_PATH ".vim"
 
     echo -e "\tinstall .vimrc"
-    ln -s $VIM_SRC $VIMRC_PATH
+    ln -s $SRC_PATH $DST_PATH
     echo -e "\tinstall .vim"
     _vim_plug=$GIT_RAW_URL/junegunn/vim-plug/master/plug.vim
     curl -sfLo $VIMDIR_PATH/autoload/plug.vim --create-dirs $_vim_plug
+    echo "done"
+}
+
+install_emacs() {
+    echo "install emacs"
+
+    SRC_PATH="$(pwd)/emacs.d"
+    DST_PATH="${HOME}/.emacs.d"
+
+    check_already $DST_PATH $SRC_PATH
+    if [ $? -eq 1 ]
+    then 
+        echo "already install"
+        return
+    fi
+
+    backup_dir $DST_PATH ".emacs.d"
+
+    echo -e "\tinstall .emacs.d"
+    ln -s $SRC_PATH $DST_PATH
     echo "done"
 }
 
@@ -72,6 +92,8 @@ install_all() {
     echo "install all"
     echo "####################"
     install_vim
+    echo "####################"
+    install_emacs
     echo "####################"
     echo "done"
 
@@ -89,6 +111,9 @@ main() {
         ;;
     "vim")
         install_vim
+        ;;
+    "emacs")
+        install_emacs
         ;;
     esac
 }
