fb32d84d721308492e2e13037b5193eba64447db
chyuwei <weichangyuwcy@gmail.com>
2024-01-25 01:55:35 +0800

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,12 +7,23 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     tab_bar_at_bottom = true,
 
+    -- window
+    window_decorations = 'RESIZE',
     window_background_opacity = 0.7,
     text_background_opacity = 0.7,
+    macos_window_background_blur = 70,
+    window_padding = {
+	left = 15,
+	right = 15,
+	top = 15,
+	bottom = 5,
+    },
 
+    -- font
     font = wezterm.font({
         family = "FiraCode Nerd Font",
         harfbuzz_features = {"calt=1", "clig=1", "liga=0"},
     }),
-    font_size = 14.5,
+    font_size = 16,
+    adjust_window_size_when_changing_font_size = false,
 }
