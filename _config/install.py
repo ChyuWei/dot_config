f080f289864a7b55e4cc91e196d03acecc5b2b10
chyuwei <weichangyuwcy@gmail.com>
2026-05-14 17:05:15 +0800

    tmux: move PREFIX indicator to right of status bar
    - Fix PREFIX indicator position: moved from status-left to status-right
    - Fix color bleed: use #[default] before time to reset background
    - Fix style syntax: use space-separated attrs inside #{?} to avoid comma parsing issue
    - install.py: use absolute SCRIPT_DIR path, add idempotent check, add brew_install/install_tmux_deps for macism

--- _config/install.py
+++ _config/install.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import subprocess
 
 def check_exist(path):
     return os.path.lexists(path)
@@ -16,12 +17,30 @@ def make_softlink(src, dst):
     os.symlink(src, dst)
     print(f'create softlink {src} -> {dst}')
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def install(app):
+    src = os.path.join(SCRIPT_DIR, app)
     install_path = os.path.expanduser(f'~/.config/{app}')
+    if os.path.islink(install_path) and os.readlink(install_path) == src:
+        print(f'{install_path} already linked to {src}, skip')
+        return
     make_backup(install_path)
-    make_softlink(os.path.abspath(f'./{app}'), os.path.expanduser(f'~/.config/{app}'))
+    make_softlink(src, install_path)
+
+def brew_install(formula):
+    if shutil.which(formula.split('/')[-1]):
+        print(f'{formula} already installed')
+        return
+    print(f'brew install {formula}')
+    subprocess.run(['brew', 'install', formula], check=True)
+
+def install_tmux_deps():
+    # macism: switch input method to ABC on tmux prefix (see tmux.conf)
+    brew_install('laishulu/homebrew/macism')
 
 if __name__ == '__main__':
     install('ghostty')
     install('tmux')
+    install_tmux_deps()
 
